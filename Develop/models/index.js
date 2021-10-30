dfdb4962530c1ce58a65cfebf77b6ba803723343
Your Name <shelbyfayemo@gmail.com>
2021-10-30 16:05:18 -0400

    executed assosciation methods

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,17 +1,26 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
+const { transformGeom2D } = require("ol/geom/SimpleGeometry");
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, { foreignKey: "category_id" });
 // Categories have many Products
-
+Category.hasMany(Product, { foreignKey: "category_id" });
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: "tag_products",
+  foreignKey: "product_id",
+});
 // Tags belongToMany Products (through ProductTag)
-
+transformGeom2D.belongsToMany(Products, {
+  through: ProductTag,
+  as: "tag_products",
+  foreignKey: "tag_id",
+});
 module.exports = {
   Product,
   Category,
